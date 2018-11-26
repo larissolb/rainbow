779f14b01074ce6665d30cffd6dab10f697696a1
larissolb <fasolya@gmail.com>
2018-11-27 00:08:29 +0300

    добавлена страница about.html

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ sliderInit('slider');
 
 /*1200x600*/
 
-sliderAdd('https://127288-365065-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2015/11/header-image.jpg', 'null');
-sliderAdd('https://ih0.redbubble.net/cover.609226.1200x300.jpg', 'main');
-sliderAdd('https://media.self.com/photos/57d88804f71ce8751f6b4858/2:1/pass/lisa-frank-coloring-fb.jpg', 'first');
-sliderAdd('https://twitchetts.com/wp-content/uploads/2016/03/2eggs.png', 'second');
+sliderAdd('../img/slide2.jpg', 'null');
+sliderAdd('../img/slide.jpg', 'main');
+sliderAdd('../img/slide1.jpeg', 'first');
+sliderAdd('../img/slide3.jpg', 'second');
 
 
 
