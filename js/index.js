9b18d052123765b96e7b13eb0844c69aa9dbce6d
larissolb <42378253+larissolb@users.noreply.github.com>
2018-11-26 17:08:36 +0300

    ссылка на внешние картинки
    нужны картинки для добавления формата 1200х600

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 sliderInit('slider');
 
-sliderAdd('../img/slide1.jpeg', 'Первый слайд');
-sliderAdd('../img/slide2.jpg', 'Второй слайд');
-sliderAdd('../img/slide3.png', 'Третий слайд');
+/*1200x600*/
+
+sliderAdd('https://127288-365065-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2015/11/header-image.jpg', 'null');
+sliderAdd('https://ih0.redbubble.net/cover.609226.1200x300.jpg', 'main');
+sliderAdd('https://media.self.com/photos/57d88804f71ce8751f6b4858/2:1/pass/lisa-frank-coloring-fb.jpg', 'first');
+sliderAdd('https://twitchetts.com/wp-content/uploads/2016/03/2eggs.png', 'second');
+
+
 
 sliderRun();
 
