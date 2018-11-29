7c639c0005b2adc1a3ff88cc522e8b6a6ba0938f
larissolb <fasolya@gmail.com>
2018-11-30 00:01:59 +0300

    добавлен php файл со скриптом случайного появление картинок с левой стороны при перезагрузке страницы

--- html/share.php
+++ html/share.php
@@ -1,11 +1,8 @@
 <!DOCTYPE html>
 <!--
-Краткое описание сообщества.
-Страница выполнена на флексе, адаптивность начата. 
-Нужна доработка для моб.версий - @media
-Не получается пока адаптировать картинки
+Здесь пользователи будут иметь возможность загружать свои картинки
 -->
-<html> 
+<html>
     <head>
         <title>Rainbow - let's this world colour!</title>
         <meta charset="UTF-8">
@@ -14,18 +11,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" type="image/x-icon" href="../img/icon.jpg">
         <link rel="stylesheet" href="../CSS/style.css">
-        <script src="../js/slider.js"></script>
     </head>
     <body>
-    <div id="header"> <!--header-->
-        <h1 id='txt'>Welcome to Rainbow world</h1>
-    </div> <!-- final header-->
+        <div id="header"> <!--header-->
+            <h1 id='txt'>Welcome to Rainbow world</h1>
+        </div> <!-- final header-->
     <div class="top">  <!-- top -->
         <nav class="menu"> <!-- menu -->
                 <ul>
                     <li><a href="../index.html">Home</a></li>
                     <li><a href="about.html">Why I am here?</a></li>
-                    <li><a href="share.php">Share art &#10000;</a></li>
+                    <li><a href="share.html">Share art &#10000;</a></li>
                     <li><a href="https://www.instagram.com/ShareRainbow">Goods for creation</a></li>
                     <li><a href="rating.html">Watch&Rate</a>
                     <ul>
@@ -41,8 +37,8 @@
                 <li><a href="#"><img src="../img/icon/menu.png" alt="Menu"></a>
                     <ul>
                         <li><a href="../index.html">Home</a></li>
+                        <li><a href="about.html">Why I am here?</a></li>
                         <li><a href="rating.html">Watch&Rate</a></li>
-                        <li><a href="share.html">Share art &#10000;</a></li>
                         <li><a href="https://www.instagram.com/ShareRainbow">Goods for creation</a></li>
                         <li><a href="#footer">Join us</a></li>
                     </ul>
@@ -130,32 +126,107 @@
                  <a  href="#close" class="close-button" title="Закрыть"></a>
                 </div> <!-- final form register-->        
 </div> <!-- final top -->
-
-        <div class="container-about"> <!--begin container -->
-            <div class="left"> <!-- begin left pics -->
-                <img src="../img/icon2.jpg" alt="img">
-                <img src="../img/icon2.jpg" alt="img">
-                <img src="../img/icon2.jpg" alt="img">
-            </div> <!-- end left pics -->
-            <div class="center"> <!-- begin center -->
-                <div>             
-                <p class="letter-red">It's a place where people make this world more colour!</p> 
-                <p class="letter-violet">You can see and mark any paints created by usual people like you.</p>
-                <p class="letter-orange">You can join us and share your paints from your colourbooks.</p> 
-                <p class="letter-green">Your uploaded paints will be checked, demostrated and rated.</p>
-                <p class="letter-darkblue">And..maybe anybody wants to buy you creater!..</p>
-                <q>It’s up to everyone to decide what to mix...</q>
-                </div>
-            <iframe src="https://www.youtube.com/embed/4TapFg5Nxfw?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>        
-              </div><!--end center-->
-            <div class="right"> <!-- begin right pics -->
-                <img src="../img/icon2.jpg" alt="img">
-                <img src="../img/icon2.jpg" alt="img">
-                <img src="../img/icon2.jpg" alt="img">
-            </div> <!-- end right pics -->
-        </div> <!--end container -->
-        
-     <div> <!-- footer -->
+    
+      <div class="container-share"> <!--главная рамка-->
+            <div class="left"> <!--position column left main-->
+                <h2>Last updates</h2>
+<!--                <img src="../img/girl.jpg" alt="Picture must be here">-->
+                <?php
+    $images1 = array(
+        'girl.jpg',
+        'pencils.jpg',
+        'icon2.jpg',
+        'colourbook.jpg',
+        'share.jpeg',
+        'leftpic.png',
+        );
+    $image1  = $images1[array_rand($images1)];
+    $output = "<img src=\"../img/" . $image1 . "\" alt=\"random pic\" />";
+    echo $output;
+?>
+               <div><p>Finally, rainbow people, we have the place where we can share our results from our paintbooks!</p></div>
+<!--              <img src="../img/colourbook.jpg" alt="Picture must be here">-->
+                               <?php
+    $images2 = array(
+        'girl.jpg',
+        'pencils.jpg',
+        'icon2.jpg',
+        'colourbook.jpg',
+        'share.jpeg',
+        'leftpic.png',
+    );
+    $image2  = $images2[array_rand($images2)];
+    $output = "<img src=\"../img/" . $image2 . "\" alt=\"random pic\" />";
+    echo $output;
+?>
+              <div><p><a href='#authorization'>Join us!</a>  Share your pics all the world ;-)</p></div>
+                </div> <!--end share left side -->
+<!--form for upload - center 70% -->
+            <div class="center">
+                <form action="#" method="post" autocomplete="on" enctype="multipart/form-data" name="Upload">
+                    <fieldset class="field"> 
+                        <div class="item">
+                        <label for="nameBook">Name of coloring-book's</label>
+                        <input id="nameBook" type="text" autofocus>
+                        </div>
+                    <div class="item" id="theme">
+                        <label for="theme">What theme?</label>
+                        <label><input type="radio" name='theme' value='Nature'>Nature</label>
+                        <label><input type="radio" name='theme' value='Space' >Space</label>
+                        <label><input type="radio" name='theme' value='Animals'>Animals</label>
+                        <label><input type="radio" name='theme' value='Cars'>Cars</label>
+                        <label><input type="radio" name='theme' value='Cities'>Cities</label>
+                        <label><input type="radio" name='theme' value='Other' checked>Other</label>
+                    </div> 
+                    <div class="item-mobile">
+                        <label for="themeM">What theme?</label>
+                        <select id="themeM" required> 
+                            <option value="Nature"> Nature</option> 
+                            <option selected value="Space">Space</option> 
+                            <option value="Animals">Animals</option> 
+                            <option value="Cars">Cars</option>
+                            <option value="Cities">Cities</option>
+                            <option value="Other">Other</option>
+                        </select> 
+                    </div>
+                    <div class="item">
+                        <label for="type">What instruments did you use?</label>
+                        <select id="type" required> 
+                            <option value="pen"> colour pens</option> 
+                            <option selected value="pencil">colour pencils</option> 
+                            <option value="gouache">gouache</option> 
+                            <option value="watercolour">watercolour</option>
+                            <option value="markers">markers</option>
+                        </select> 
+                    </div>
+                    <div class="item">
+                        <label for="amount">How many colours did you use?</label> 
+                       <input id="amount" type="number" min="1" step="1">
+                    </div>
+                    <div>
+                        <label for="describe">&#9999; Describe it</label>
+                        <textarea id="describe" placeholder="I choosed this pic because..." cols="70" rows="3" maxlength="100"></textarea>
+                    </div>  
+                    <fieldset id="share">
+                    <div>
+                        <label for="pics">Upload your best picture</label>
+                        <input id="pics" type="file" multiple accept="image/*">
+                    </div>
+                    </fieldset>
+                    <fieldset class="button-shareit">
+                        <input type="submit" value="Share" id='shareBtn' class="Btn">
+                        <input type="reset" value="Reset" class="Btn">
+                    </fieldset>  
+                    </fieldset>  
+                </form>
+                <div><img src="../img/horizont_adv.png" alt='horizontal adv'></div>
+           </div>    
+            <div class="right"> <!-- begin share right side -->
+                <img src="../img/ad.png" alt="advertising">
+            </div><!-- begin share right side -->
+       </div> <!--конец главной рамки-->
+      
+    <div> <!-- footer -->
         <div id='footer' class="footer">
             <div class="socnet">
                 <a href="https://www.whatsapp.com" target="_blank">
@@ -179,7 +250,8 @@
     </div>     <!-- finish footer -->          
                 
 <!--      скрипты          -->
-<script src="../js/header.js"></script>
-<script src="../js/users.js"></script>
+       <script src="../js/header.js"></script>
+       <script src="../js/users.js"></script>
+       <script src="../js/collection.js"></script>
     </body>
 </html>
