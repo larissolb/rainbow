45001ab7c75f9ffbb5474923e4076e28e4d871d6
larissolb <42378253+larissolb@users.noreply.github.com>
2019-01-30 13:51:12 +0300

    Update PicModel.php
    add method getPicsByType for different categories

--- private/Models/PicModel.php
+++ private/Models/PicModel.php
@@ -130,6 +130,49 @@ protected $response;
     }
     
 }
+  //get pics for different types of instruments
+   public function getPicsByType($id, $type) {
+        $sql = "SELECT id, nameBook, amount, text, img_path, Themes_id, Types_id, Users_login  FROM Pics WHERE id=:id AND Types_id:=Types_id";
+        $params = [
+            'id'=>$id,
+            'Types_id'=>$type
+                ];
+        $pic_arr = $this->DBConnection->execute($sql, $params, true);
+        
+  foreach ($pic_arr as $pics) { 
+        //choose theme
+        $id_theme = $pics['Themes_id'];   
+        $sql = "SELECT theme FROM Themes WHERE id=:id";
+        $params = [
+            'id'=>$id_theme
+                ];
+        $theme_arr = $this->DBConnection->execute($sql, $params, true);
+        foreach ($theme_arr as $key => $value) {
+            $theme = $value["theme"];            
+        }
+        $pics['Themes_id'] = $theme;
+        
+        //choose instrument
+        $id_type = $pics['Types_id'];   
+        $sql_type = "SELECT type FROM Types WHERE id=:id";
+        $params_type = [
+            'id'=>$id_type
+                ];
+        $type_arr = $this->DBConnection->execute($sql_type, $params_type, true);
+        foreach ($type_arr as $key => $value) {
+            $type = $value["type"];
+        }
+         $pics['Types_id'] = $type;
+      
+        if ($pics["id"] == $id){
+            $_SESSION['idPics'] =  $pics['id'];
+            return $pics;       
+        }  
+    }
+    
+}
+ 
+  
  
    //check size and type
     public function loadPics($data) {
